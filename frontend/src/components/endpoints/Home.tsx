0ef1c1371388bf8e5e688e521f2a92990d6afee6
Frankllin <frankllin15@hotmail.com>
2022-07-05 22:23:09 -0300

    mudança temporaria

--- frontend/src/components/endpoints/Home.tsx
+++ frontend/src/components/endpoints/Home.tsx
@@ -1,7 +1,55 @@
-import { Post, usePostsQuery } from "../../lib/graphql/generated";
+import {
+  Post,
+  PostQuery,
+  PostsQuery,
+  usePostsQuery,
+} from "../../lib/graphql/generated";
 import { NewPost } from "../NewPost";
 import graphqlClient from "../../lib/graphql/graphqlClient";
 import { PostItem } from "../PostItem";
+
+type MainContentProps = {
+  data: PostsQuery | undefined;
+  isLoading: boolean;
+};
+
+const MainContent = ({ data, isLoading }: MainContentProps) => {
+  if (isLoading) {
+    return (
+      <h2 className="text-white font-semibold text-center text-2xl md:text-3xl">
+        Carregando...
+      </h2>
+    );
+  }
+
+  if (data?.posts.__typename === "PostList") {
+    if (data.posts.items?.length! > 0 &&  data.posts.items instanceof Array<Post>) {
+      return (
+        <div> 
+          {
+            data.posts.items.map(
+              (post: any) => 
+              <PostItem key={post.id} post={post} />
+            
+              )
+            }
+          </div>
+        )
+    } else {
+      return (
+        <h2 className="text-white font-semibold text-center text-2xl md:text-3xl">
+          Nenhum post encontrado. Seja o primeiro a publicar!
+        </h2>
+      );
+    }
+  }
+
+  return (
+    <h2 className="text-white font-semibold text-center text-2xl md:text-3xl">
+      Um erro inesperado ocorreu.
+    </h2>
+  );
+};
 
 export const Home = () => {
   const { data, isLoading } = usePostsQuery(
@@ -19,8 +67,10 @@ export const Home = () => {
       </h1>
       <NewPost />
       <div className="w-full">
-        {isLoading ? (
-          <div>Carregando...</div>
+        {/* {isLoading ? (
+          <h2 className="text-white font-semibold text-center text-2xl md:text-3xl">
+            Carregando...
+          </h2>
         ) : (
           data?.posts.__typename === "PostList" &&
           (data.posts.items?.length! > 0 ? (
@@ -32,7 +82,9 @@ export const Home = () => {
               Nenhum post encontrado. Seja o primeiro a publicar!
             </h2>
           ))
-        )}
+        )} */}
+
+        <MainContent data={data} isLoading={isLoading} />
       </div>
     </div>
   );
