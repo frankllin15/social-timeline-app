d0c326d65547baf0d47052c8fcb8bc2b797f658d
Frankllin <frankllin15@hotmail.com>
2022-07-06 00:54:32 -0300

    fix: correção de texto

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
 export const Footer = () => {
   return (
     <footer className="w-full text-white bg-zinc-900  p-4 md:px-12 lg:px-20">
       {/* <div> */}
       <p className="text-end">
-        © {new Date().getFullYear()}, Developed whith{" "}
+        © {new Date().getFullYear()}, Developed with{" "}
         <span className="text-red-600">&#9829;</span> by{" "}
         <a
           className="font-semibold"
